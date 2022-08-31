8c6da6e61c89da8421da3cd48a033daac0b594d0
Doug <douglas.rice@salesforce.com>
2022-08-31 08:38:22 -0400

    Adding Timesheets Controller

--- src/lwc/approveOrRejectTimesheetsTable/approveOrRejectTimesheetsTable.js
+++ src/lwc/approveOrRejectTimesheetsTable/approveOrRejectTimesheetsTable.js
@@ -2,4 +2,9 @@ import { LightningElement, api } from 'lwc';
 
 export default class ApproveOrRejectTimesheetsTable extends LightningElement {
     @api timesheets;
+    modalShown = false;
+
+    toggleModal() {
+        this.modalShown = !this.modalShown;
+    }
 }
