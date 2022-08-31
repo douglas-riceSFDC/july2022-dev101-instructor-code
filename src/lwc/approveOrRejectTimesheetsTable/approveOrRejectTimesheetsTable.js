4a0272fe672cb7de7a47a177173ac64a9084dbc0
Doug <douglas.rice@salesforce.com>
2022-08-31 15:00:02 -0400

    Adding buttons

--- src/lwc/approveOrRejectTimesheetsTable/approveOrRejectTimesheetsTable.js
+++ src/lwc/approveOrRejectTimesheetsTable/approveOrRejectTimesheetsTable.js
@@ -3,6 +3,28 @@ import { LightningElement, api } from 'lwc';
 export default class ApproveOrRejectTimesheetsTable extends LightningElement {
     @api timesheets;
     modalShown = false;
+    selectedTimesheets = [];
+
+    columns = [
+        { label: 'Name', fieldName: 'Name' },
+        { label: 'Status', fieldName: 'Status__c' },
+        { label: 'Times Rejected', fieldName: 'Number_of_Times_Rejected__c' }
+    ];
+
+    handleSelectedRows(event) {
+        let selectedRows = event.detail.selectedRows;
+        console.log(JSON.parse(JSON.stringify(selectedRows)));
+
+        this.selectedTimesheets = selectedRows;
+    }
+
+    approveTimesheets() {
+        // approve the selected rows
+    }
+
+    rejectTimesheets() {
+
+    }
 
     toggleModal() {
         this.modalShown = !this.modalShown;
